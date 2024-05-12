8b34b21e529dd9a675c074c28daee0ded3ebd9dc
inaddis <145146147+inaddis@users.noreply.github.com>
2024-05-12 17:08:56 -0600

    Update iterative_heap_sort.cpp

--- iterative_heap_sort.cpp
+++ iterative_heap_sort.cpp
@@ -1,10 +1,39 @@
 #include "kary_heap.cpp"
 
+void iterativeHeapSort(int arr[], int size) {
+    KaryHeap heap(2); 
+    for (int i = 0; i < size; ++i)
+        heap.insert(arr[i]);
 
-  int main() {
+    for (int i = 0; i < size; ++i)
+        arr[i] = heap.deleteMin();
+}
 
-  }
+int main() {
+    int arr[] = {5, 2, 9, 1, 7};
+    int size = sizeof(arr) / sizeof(arr[0]);
 
-  void iterativeHeapSort() {
+    int arr1[] = {12, 7, 9, 15, 4};
+    int size1 = sizeof(arr1) / sizeof(arr1[0]);
 
-  }
+    int arr2[] = {23, 56, 32, 11, 8, 2, 17};
+    int size2 = sizeof(arr2) / sizeof(arr2[0]);
+
+    iterativeHeapSort(arr, size);
+
+    std::cout << "Sorted array: ";
+    for (int i = 0; i < size; ++i)
+        std::cout << arr[i] << " ";
+    std::cout << std::endl;
+
+    std::cout << "Sorted array: ";
+    for (int i = 0; i < size; ++i)
+        std::cout << arr1[i] << " ";
+    std::cout << std::endl;
+
+    std::cout << "Sorted array: ";
+    for (int i = 0; i < size; ++i)
+        std::cout << arr2[i] << " ";
+    std::cout << std::endl;
+
+}
